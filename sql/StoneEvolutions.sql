80410816bf7e2355a231c7b0cb8927d84992e580
Nick Wu <nickwu241@gmail.com>
2018-11-08 18:21:09 -0800

    Add csv, sql for StoneEvolutions

---
+++ sql/StoneEvolutions.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Nov 09, 2018 at 02:19 AM
+-- Server version: 5.6.42
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pacificpoke`
+--
+
+--
+-- Dumping data for table `StoneEvolutions`
+--
+
+INSERT INTO `StoneEvolutions` (`IID`, `EvolveToPID`) VALUES
+(33, 26),
+(10, 31),
+(10, 34),
+(10, 36),
+(32, 38),
+(10, 40),
+(47, 45),
+(32, 59),
+(34, 62),
+(47, 71),
+(34, 91),
+(47, 103),
+(34, 121),
+(34, 134),
+(33, 135),
+(32, 136);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
