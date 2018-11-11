bf711c1225cc7a86dc0ca339725898ac603c0eac
Nick Wu <nickwu241@gmail.com>
2018-11-11 03:00:23 -0800

    Add csv, sql for PokemonSellsItems

--- reset_db.sh
+++ reset_db.sh
@@ -14,4 +14,5 @@ mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/Locations.sql
 mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/Buildings.sql
 mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/Gyms.sql
 mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/Pokemart.sql
+mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/PokemartSellsItems.sql
 mysql -h 127.0.0.1 --user root --password=root pacificpoke < sql/StoneEvolutions.sql
---
+++ sql/PokemartSellsItems.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Nov 11, 2018 at 10:58 AM
+-- Server version: 5.6.42
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pacificpoke`
+--
+
+--
+-- Dumping data for table `PokemartSellsItems`
+--
+
+INSERT INTO `PokemartSellsItems` (`IID`, `BuildingName`) VALUES
+(3, 'Celadon City Department Store'),
+(11, 'Celadon City Department Store'),
+(12, 'Celadon City Department Store'),
+(13, 'Celadon City Department Store'),
+(14, 'Celadon City Department Store'),
+(15, 'Celadon City Department Store'),
+(17, 'Celadon City Department Store'),
+(18, 'Celadon City Department Store'),
+(19, 'Celadon City Department Store'),
+(20, 'Celadon City Department Store'),
+(30, 'Celadon City Department Store'),
+(32, 'Celadon City Department Store'),
+(33, 'Celadon City Department Store'),
+(34, 'Celadon City Department Store'),
+(35, 'Celadon City Department Store'),
+(36, 'Celadon City Department Store'),
+(37, 'Celadon City Department Store'),
+(38, 'Celadon City Department Store'),
+(39, 'Celadon City Department Store'),
+(46, 'Celadon City Department Store'),
+(47, 'Celadon City Department Store'),
+(51, 'Celadon City Department Store'),
+(53, 'Celadon City Department Store'),
+(55, 'Celadon City Department Store'),
+(56, 'Celadon City Department Store'),
+(58, 'Celadon City Department Store'),
+(60, 'Celadon City Department Store'),
+(61, 'Celadon City Department Store'),
+(62, 'Celadon City Department Store'),
+(65, 'Celadon City Department Store'),
+(66, 'Celadon City Department Store'),
+(67, 'Celadon City Department Store'),
+(68, 'Celadon City Department Store'),
+(4, 'Cerulean City Pokemart'),
+(11, 'Cerulean City Pokemart'),
+(12, 'Cerulean City Pokemart'),
+(14, 'Cerulean City Pokemart'),
+(15, 'Cerulean City Pokemart'),
+(20, 'Cerulean City Pokemart'),
+(30, 'Cerulean City Pokemart'),
+(2, 'Cinnabar Island Pokemart'),
+(3, 'Cinnabar Island Pokemart'),
+(18, 'Cinnabar Island Pokemart'),
+(29, 'Cinnabar Island Pokemart'),
+(52, 'Cinnabar Island Pokemart'),
+(53, 'Cinnabar Island Pokemart'),
+(57, 'Cinnabar Island Pokemart'),
+(2, 'Fuchsia City Pokemart'),
+(3, 'Fuchsia City Pokemart'),
+(19, 'Fuchsia City Pokemart'),
+(52, 'Fuchsia City Pokemart'),
+(53, 'Fuchsia City Pokemart'),
+(56, 'Fuchsia City Pokemart'),
+(2, 'Indigo Plateau Pokemart'),
+(3, 'Indigo Plateau Pokemart'),
+(16, 'Indigo Plateau Pokemart'),
+(17, 'Indigo Plateau Pokemart'),
+(52, 'Indigo Plateau Pokemart'),
+(53, 'Indigo Plateau Pokemart'),
+(57, 'Indigo Plateau Pokemart'),
+(3, 'Lavender Town Pokemart'),
+(11, 'Lavender Town Pokemart'),
+(12, 'Lavender Town Pokemart'),
+(13, 'Lavender Town Pokemart'),
+(15, 'Lavender Town Pokemart'),
+(19, 'Lavender Town Pokemart'),
+(29, 'Lavender Town Pokemart'),
+(53, 'Lavender Town Pokemart'),
+(56, 'Lavender Town Pokemart'),
+(4, 'Pewter City Pokemart'),
+(11, 'Pewter City Pokemart'),
+(12, 'Pewter City Pokemart'),
+(14, 'Pewter City Pokemart'),
+(15, 'Pewter City Pokemart'),
+(20, 'Pewter City Pokemart'),
+(29, 'Pewter City Pokemart'),
+(3, 'Saffron City Pokemart'),
+(18, 'Saffron City Pokemart'),
+(29, 'Saffron City Pokemart'),
+(52, 'Saffron City Pokemart'),
+(53, 'Saffron City Pokemart'),
+(57, 'Saffron City Pokemart'),
+(4, 'Vermilion City Pokemart'),
+(13, 'Vermilion City Pokemart'),
+(14, 'Vermilion City Pokemart'),
+(15, 'Vermilion City Pokemart'),
+(19, 'Vermilion City Pokemart'),
+(30, 'Vermilion City Pokemart'),
+(4, 'Viridian City Pokemart'),
+(11, 'Viridian City Pokemart'),
+(12, 'Viridian City Pokemart'),
+(15, 'Viridian City Pokemart');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
