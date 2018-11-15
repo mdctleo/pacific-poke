765c6a5dd6d94316464d6e326c5f517e5a8b1f8b
mdctleo <31061195+mdctleo@users.noreply.github.com>
2018-11-14 21:57:49 -0800

    updated router

--- src/PokemonRouter.ts
+++ src/PokemonRouter.ts
@@ -25,94 +25,118 @@ router.get('/pokemon', (req: any, res: any, next: any) => {
 router.get('/getPokemonWithName/:name', (req: any, res: any, next: any) => {
     pokemonImpl.selectPokemonWithName(req.params.name)
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // SELECT all Pokemon names that are fire-type that have the move “Bite”.
 router.get('/getPokemonWithTypeAndMove/:type/:move', (req: any, res: any, next: any) => {
-    // TODO: replace with actual implementation
-    pokemonImpl.selectPokemonWithName(req.params.move)
+    pokemonImpl.getPokemonWithTypeAndMove(req.params.type, req.params.move)
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // SELECT all Pokemarts where “FireStone” is sold.This query would join 6 tables including Locations, Buildings, Pokemart, PokemartSellsItems, Items and Stone. Used by Pokemon Trainers.
 router.get('/getPokemartWithItem/:item', (req: any, res: any, next: any) => {
-    // TODO: replace with actual implementation
-    pokemonImpl.selectPokemonWithName(req.params.item)
+    pokemonImpl.getPokemartWithItem(req.params.item)
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // SHOW how many pokemons of each type exist in the database.
 router.get('/getPokemonsWithType', (req: any, res: any, next: any) => {
     // TODO: replace with actual implementation
-    pokemonImpl.selectPokemonWithName(req.params.type)
+    pokemonImpl.getPokemonsWithType()
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // SELECT Moves.Effect FROM Moves WHERE Moves.MoveName = ?;
 router.get('/getMoveWithName/:name', (req: any, res: any, next: any) => {
     // TODO: replace with actual implementation
-    pokemonImpl.selectPokemonWithName(req.params.name)
+    pokemonImpl.getMoveWithName(req.params.name)
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // SELECT PokemonEvolvesTo.EvolveToPID FROM PokemonEvolvesTo WHERE PokemonEvolvesTO.EvolveFromPID = ?;
 router.get('/getEvolutionWithId/:id', (req: any, res: any, next: any) => {
     // TODO: replace with actual implementation
-    pokemonImpl.selectPokemonWithName(req.params.id)
+    pokemonImpl.getEvolutionWithId(req.params.id)
         .then((result) => {
-            res.send(result);
+            res.status(200).send(result);
         })
         .catch((err) => {
-            res.send(err);
+            res.status(400).send(err);
         });
 });
 
 // A Division Query: Get the IID and ItemName of items that are sold at every pokemart.
 router.get('/getItemsSoldAtEveryPokemart/:id', (req: any, res: any, next: any) => {
+    pokemonImpl.getItemsSoldAtEveryPokemart(req.params.id)
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 
 });
 
 // INSERT a new item with its name, effect, cost and which Pokemart it’s sold in, used by Pokemart Inc.
 router.post('/newItem', (req: any, res: any) => {
-    // TODO: replace with actual implementation
-    // parameters should be req.body.name, req.body.effect, req.body.cost, req.body.pokemart
+    // parameters should be req.body.id, req.body.name, req.body.effect, req.body.cost
+    pokemonImpl.insertItem(req.body.id, req.body.name, req.body.effect, req.body.cost)
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 
 });
 
 // DELETE a building WHERE it’s ID matches the buildings ID to be deleted. For example, if it has been demolished. Used by Pokemon Scholar.
 router.delete('/deleteBuilding', (req: any, res: any) => {
-    // TODO: replace with actual implementation
     // parameters should be req.body.id
+    pokemonImpl.deleteBuilding(res.body.id)
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 });
 
 // UPDATE a Gym’s gym leader name WHERE the ID matches the gym’s ID to updated. For example, if the gym leader passes away, goes on parental leave or is relieved of their position. Used by Gym Owners.
 router.put('/updateGymLeader', (req: any, res: any) => {
     //TODO: replace with actual implementation
+    pokemonImpl.updateGymLeader(req.body.name, req.body.id)
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
     // parameters should be req.body.id
 });
 
@@ -120,5 +144,22 @@ router.put('/updateGymLeader', (req: any, res: any) => {
 router.get('/view', (req: any, res: any) => {
    // no request parameters for this one, we passed all tuples from this temporary table
    // to frontend, where it will be display in a table-like style;
+    pokemonImpl.getView()
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+router.get('/getLocationsPokemonAppearsIn', (req: any, res: any) => {
+    pokemonImpl.getLocationsPokemonAppearsIn()
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 });
 module.exports = router;
