675e71a33815149c83aaeed433dd2858da48731c
Nick Wu <nickwu241@gmail.com>
2018-11-14 16:39:35 -0800

    Add /pokemon endpoint formatted

--- src/PokemonImpl.ts
+++ src/PokemonImpl.ts
@@ -1,11 +1,32 @@
 import Database from "./Database";
 
+function fmtTable(result: any): Object {
+    let columns = []
+    for (let column in result[0]) {
+        columns.push({Header: column, accessor: column});
+    }
+    return { tableColumns: columns, tableData: result };
+}
+
 export default class PokemonImpl {
     private db: any;
     private con: any;
     constructor() {
         this.db = new Database();
         this.con = this.db.database;
+    }
+
+    public selectAll(): Promise<Object> {
+        let sql = 'SELECT * FROM Pokemon';
+        return new Promise((resolve, reject) => {
+            this.con.query(sql, (err: any, result: any) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(fmtTable(result));
+                }
+            })
+        });
     }
 
     public selectPokemonWithName(name: string): Promise<string> {
